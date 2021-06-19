e3fc779f7a84e5980749e409c9ebad81a77f4fa6
EthanGahm <ethangahm@gmail.com>
2021-06-18 20:27:20 -0400

    Ignore this commit.

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io")
+const io = socket(server);
+
+io.on("connection", (socket) => {
+  socket.emit()
+});
